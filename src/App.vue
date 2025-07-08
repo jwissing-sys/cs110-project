338c3fc7128d2f5de7a49c24d1fabbe8e7c139b8
Joel Wissing <jwiss001@ucr.edu>
2025-07-08 21:28:58 +0900

    Clean up App.vue and register NavigationBar

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import NavigationBar from './components/NavigationBar.vue'
+</script>
+
+<template>
+  <NavigationBar />
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding: 2rem;
+}
+</style>
