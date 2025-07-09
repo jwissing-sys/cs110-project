b45d2c865148d1ff6688b51ce8a6c59dd419cc71
Joel Wissing <jwiss001@ucr.edu>
2025-07-10 01:41:20 +0900

    Correct API keys creating new app with firebase auth

--- src/firebaseResources.js
+++ src/firebaseResources.js
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAx9n2llF2d8Z0Ge-xuBfc-_FbPcgSJuFY",
+  apiKey: "AIzaSyAx9n2llF2d8Z0Ge-xuBfc-_FbPcgSJuFY-xuBfc-_FbPcgSJuFY",
   authDomain: "cs110-project-116aa.firebaseapp.com",
   projectId: "cs110-project-116aa",
-  storageBucket: "cs110-project-116aa.appspot.com",
+  storageBucket: "cs110-project-116aa.firebasestorage.app",
   messagingSenderId: "329992581506",
-  appId: "1:329992581506:web:079702292749d905195f9b"
+  appId: "1:329992581506:web:06cf0047dcb30aa2195f9b"
 }
 
 const app = initializeApp(firebaseConfig)
