11f245fd5bc8e0605a841c63216af3d219a966cd
Joel Wissing <jwiss001@ucr.edu>
2025-07-10 01:45:10 +0900

    Finalized Login

--- src/firebaseResources.js
+++ src/firebaseResources.js
@@ -1,13 +1,12 @@
-// src/firebaseResources.js
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAx9n2llF2d8Z0Ge-xuBfc-_FbPcgSJuFY-xuBfc-_FbPcgSJuFY",
+  apiKey: "AIzaSyAx9n2llF2d8Z0Ge-xuBfc-_FbPcgSJuFY",
   authDomain: "cs110-project-116aa.firebaseapp.com",
   projectId: "cs110-project-116aa",
-  storageBucket: "cs110-project-116aa.firebasestorage.app",
+  storageBucket: "cs110-project-116aa.appspot.com",
   messagingSenderId: "329992581506",
   appId: "1:329992581506:web:06cf0047dcb30aa2195f9b"
 }
