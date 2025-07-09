efb946668048d47adfbc99a3eaa29361e309e8c3
Joel Wissing <jwiss001@ucr.edu>
2025-07-10 01:26:12 +0900

    Final login changes API keys

--- src/firebaseResources.js
+++ src/firebaseResources.js
@@ -1,3 +1,4 @@
+// src/firebaseResources.js
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
@@ -6,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAx9n2llF2d8Z0Ge-xuBfc-_FbPcgSJuFY",
   authDomain: "cs110-project-116aa.firebaseapp.com",
   projectId: "cs110-project-116aa",
-  storageBucket: "cs110-project-116aa.firebasestorage.app",
+  storageBucket: "cs110-project-116aa.appspot.com",
   messagingSenderId: "329992581506",
   appId: "1:329992581506:web:079702292749d905195f9b"
 }
